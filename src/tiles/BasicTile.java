dd12cd5b0b497229f3619e0e83286ae8bbd13bbc
alexvonstangecvuhs <alexandervonstange@cvuhs.org>
2014-06-01 14:55:30 -0400

    Adding spikes

--- src/tiles/BasicTile.java
+++ src/tiles/BasicTile.java
@@ -7,13 +7,17 @@ public class BasicTile extends Tile{
 
 	protected int tileId;
 	protected int tileColor;
+	
+	/* id is for determining which block is which
+	 x and y are for creating the id
+	 tileColor is what the color is on the actual image
+	 levelColor is what we want it to display as */
 
 	public BasicTile(int id, int x, int y, int tileColor, int levelColor) {
 		super(id, false, false, levelColor);
 		this.tileId = x + y;
 		this.tileColor = tileColor;
 	}
-
 
 	public void render(Screen screen, Level level, int x, int y) {
 		screen.render(x, y, tileId, tileColor, 0x00, 1);
